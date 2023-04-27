62af7a293977ae05d6ba056fc294e40dc7bc02ea
Ulisses Carvalho <ulisses@CMU.LOCAL>
2023-04-27 11:01:55 -0300

    Seção 8: Angular - Reactive Forms e Mudança de Layout

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { EventoService } from '../../services/evento.service';
-import { Evento } from '../../models/Evento';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/validatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  get f(): any {return  this.form.controls; }
+
+  OnSubmit(): void {
+    if(this.form.invalid)
+      return;
   }
 
+  ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch("senha", "confirmarSenha")
+    }
+
+    this.form = this.fb.group({
+      titulo: ["", Validators.required],
+      primeiroNome: ["", Validators.required],
+      ultimoNome: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      telefone: ["", Validators.required],
+      funcao: ["", Validators.required],
+      descricao: ["", Validators.required],
+      senha: ["", [Validators.required, Validators.minLength(6)]],
+      confirmarSenha: ["", Validators.required]
+    }, formOptions);
+  }
+
+  public resetForm(event: any): void {
+    event.preventDefault();
+    this.form.reset();
+  }
 }
